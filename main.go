ed369cb183489986c6c346b143a45765c418b8b6
Abdul Rehman <arehmandev@outlook.com>
2017-09-21 15:42:58 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func main() {
+	fmt.Println("placeholder")
+}
+
+// Search - this is a function for searching a directory for a file/folder/both
+func Search(pattern, pathforsearching, filetype string) []string {
+
+	var files []string
+
+	if filetype == "all" {
+		filepath.Walk(pathforsearching, func(path string, f os.FileInfo, _ error) error {
+			// if !f.IsDir() {
+			r, err := regexp.MatchString(pattern, f.Name())
+			if err == nil && r {
+				absolutefilepath, err := filepath.Abs(path)
+				if err != nil {
+					log.Fatal(err)
+				}
+				files = append(files, absolutefilepath)
+				// }
+			}
+			return nil
+		})
+	}
+
+	if filetype == "file" {
+		filepath.Walk(pathforsearching, func(path string, f os.FileInfo, _ error) error {
+			if !f.IsDir() {
+				r, err := regexp.MatchString(pattern, f.Name())
+				if err == nil && r {
+					absolutefilepath, err := filepath.Abs(path)
+					if err != nil {
+						log.Fatal(err)
+					}
+					files = append(files, absolutefilepath)
+				}
+			}
+			return nil
+		})
+	}
+
+	if filetype == "folder" {
+		filepath.Walk(pathforsearching, func(path string, f os.FileInfo, _ error) error {
+			if f.IsDir() {
+				r, err := regexp.MatchString(pattern, f.Name())
+				if err == nil && r {
+					absolutefilepath, err := filepath.Abs(path)
+					if err != nil {
+						log.Fatal(err)
+					}
+					files = append(files, absolutefilepath)
+				}
+			}
+			return nil
+		})
+	}
+
+	return files
+}
